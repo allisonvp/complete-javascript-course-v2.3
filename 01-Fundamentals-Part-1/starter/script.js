e20d5344de9e93bf2767eba05bb7decef433830d
Allison Velarde <a.velardep@pucp.edu.pe>
2025-05-28 19:53:41 -0500

    feat: 03-Developer-Skills

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,30 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+console.log("-----------Values and Variables-----------");
 
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+/* Variable name conventions: no pueden comenzar con un numero, no palabras reservadas. Contienen solo letras, numeros, guiones bajos y signo dolar */
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+
+console.log("-----------Data Types-----------");
---
+++ Assignments/script.js
@@ -0,0 +1,16 @@
+/ Values and Variables */;
+/* 
+1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+2. Log their values to the console. 
+**/
+
+let country = "Peru";
+let continent = "America Latina";
+let population = 33.85;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/ Data Types */;
